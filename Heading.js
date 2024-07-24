f86a83dd7db2b6e366b006b2f2797f4569529af5
Dhruv Gupta <dhruvgupta888@gmail.com>
2024-07-24 18:52:45 +0530

    React Componets Basics

---
+++ Heading.js
@@ -0,0 +1,8 @@
+export default function Heading({ heading, name }) {
+  return (
+    <>
+      <h1>{heading}</h1>
+      <p>My name is {name}</p>
+    </>
+  );
+}
--- app.js
+++ app.js
@@ -1,32 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-// for nested html elements
-/**
- * <div id="parent">
- *   <div id="child1">
- *      <h1>I'm h1 tag</h1>
- *      <h2>I'm h2 tag</h2>
- *   </div>
- *  <div id="child2">
- *      <h1>I'm h1 tag</h1>
- *      <h2>I'm h2 tag</h2>
- *   </div>
- * </div>
- *
- * ReactElement(Object) => HTML(Browser Understands)
- */
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-]);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+import Heading from "./Heading";
+const yo = "Yo Wassup Bro !!";
+const user = "Dhruv";
+export default function App() {
+  return (
+    <>
+      <Heading heading={yo} name={user} />
+    </>
+  );
+}
